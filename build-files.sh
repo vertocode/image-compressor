167b220d7ab1e61003b53100735f7d1dd59dcbee
vertocode <evertonvanoni1@gmail.com>
2024-10-15 18:39:07 -0300

    feat: tyr

---
+++ build-files.sh
@@ -0,0 +1,6 @@
+echo "Migrando banco de dados..."
+python3 manage.py makemigrations --noinput
+python3 manage.py migrate --noinput
+
+echo "Coletando arquivos estáticos..."
+python3 manage.py collectstatic --noinput
